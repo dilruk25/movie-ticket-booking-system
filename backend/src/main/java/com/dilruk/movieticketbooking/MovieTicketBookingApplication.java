05ba7070f67d165c0f44f43a999b75dad4c84116
dilruk25 <dilrukjayasinghe25@outlook.com>
2024-11-10 09:24:14 +0530

    Add CLI, backend, and frontend components to the same repository

--- backend/src/main/java/com/dilruk/movieticketbooking/MovieTicketBookingApplication.java
+++ backend/src/main/java/com/dilruk/movieticketbooking/MovieTicketBookingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieticketbookingApplication {
+public class MovieTicketBookingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieticketbookingApplication.class, args);
+		SpringApplication.run(MovieTicketBookingApplication.class, args);
 	}
 
 }
